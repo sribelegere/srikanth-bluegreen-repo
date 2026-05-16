bf6a04c2cd59916d0ab256ca1049c4319dc12cf0
nkamlesh <46484920+nkamlesh@users.noreply.github.com>
2026-05-16 15:55:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1 style="color:blue">BLUE version - v1.0</h1>');
+  res.end('<h1 style="color:green">Green version - v2.0</h1>');
 });
 server.listen(80, () => console.log('Server running on port 80'));
